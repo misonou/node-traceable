b852771118f6be2118b0c71404992b9eead430dd
Takeru <takeru@Lees-iMac.local>
2015-10-24 02:22:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,303 @@
+# traceable
+
+Capture, parse and format V8 stack trace.
+
+## Installation
+
+```
+npm install traceable
+```
+
+## Usage
+
+### traceable([input], [options])
+
+The main function can:
+-   Capture and normalize the stack trace from the current invocation
+-   Parse stack trace from an `Error` object
+-   Recongize `asyncStack` on an `Error` object
+-   Format the stack trace
+
+The returned object is an instance of `StackTrace`.
+
+```javascript
+var traceable = require('traceable');
+
+// main function is aliased as a property of the module
+traceable.trace === traceable;
+
+// get stack trace
+traceable();
+
+// get stack trace with top most N frames skipped
+traceable(2);
+
+// get stack trace with frames above myFunc skipped
+traceable(myFunc);
+
+// get stack trace captured by the Error object
+var err = new Error();
+traceable(err);
+
+// same as above in case only the stack trace itself is available
+var stack = err.stack
+traceable(stack);
+```
+
+#### Options
+
+Below is an exhausive list of options, with the default value shown.
+
+```javascript
+{
+    // string prepended to each frame
+    // if supplied with number, that number of spaces is prepended
+    indent: "    ",
+
+    // hide frames from source files specified (see 'Blackboxing')
+    blackbox: [],
+
+    // format the stack trace when StackTrace.toString is called
+    formatter: Formatter.default,
+
+    // descriptive label for anonymous function
+    anonString: "",
+
+    // show column number
+    showColumnNumber: false,
+
+    // show eval site if function is from eval'd code
+    showEvalOrigin: true,
+
+    // append async stack trace if any
+    showAsyncOrigin: true,
+
+    // show original source file path (see 'Paths to source files')
+    showFullPath: false,
+
+    // show original function name (see 'Function names')
+    showRawFunctionName: false
+}
+```
+
+#### Paths to source files
+
+Unless `showFullPath` is set to `true`, paths to source files are stripped as
+relative to the innermost `node_modules` folder if applicable:
+
+`/path/to/node_modules/my-mod/node_modules/traceable/traceable.js` is resolved as `traceable/traceable.js`
+
+#### Function names
+
+Unless `showRawFunctionName` is set to `true`, function names are manipulated as follow:
+
+-   function which aliased to another name takes the highest precedence
+
+    `Object.aliasedName` instead of `Object.origName [as aliasedName]`
+
+-   function names resolved with pseudo-namespaces are stripped
+
+    `Object.myFunc` instead of `Object.$.extend.myFunc`
+
+-   if the function is called on an anonymous object (often calling function of an object literal)
+    and the function is not defined on `Object.prototype`, the type name is replaced with `?`:
+
+    `?.resolve` instead of `Object.resolve`; while
+
+    `Object.hasOwnProperty` is kept
+
+#### Blackboxing
+
+Blackboxing, similar to the node inspector interface, can hide frames from certain source file
+as specified.
+
+Paths can be absolute or relative to the **innermost** node_modules folder.
+
+For node modules, `/path/to/program/node_modules/my-mod/node_modules/nested/util/x.js`
+can be blackboxed by:
+
+-   `nested/util/x.js`
+-   `nested/util`
+-   `nested`
+
+For built-in modules, specified their file names directly, e.g. `fs.js`.
+
+### traceable.StackTrace
+
+The `StackTrace` object is an `Array`-prototyped object that contains
+`StackFrame` objects parsed from the stack trace.
+
+So you can freely work with the parsed stack trace with functions like
+`Array.filter` and `Array.forEach` etc.
+
+```javascript
+var st = traceable(err);
+st.forEach(function (v) {
+    // prints the function name of each call site
+    console.log(v.functionName);
+});
+```
+
+### traceable.StackFrame
+
+The `StackFrame` object represent each call site parsed and contains
+normalized information about the call site.
+
+```javascript
+{
+    // formatted string from built-in CallSite.toString()
+    rawString: '    at new Object.extend.doSomething (/path/to/my/script.js:50:4)'
+
+    // whether the call site is native
+    native: false,
+
+    // whether the call site is called by 'new' operator
+    isConstructor: true,
+
+    // type name of 'this' object in the call site
+    typeName: 'Object',
+
+    // function name resolved by V8
+    rawFunctionName: 'extend.doSomething',
+
+    // full path of the script file
+    // null for native call site
+    fileName: '/path/to/my/script.js',
+
+    // line number of the call site
+    // null for native call site
+    lineNumber: 50,
+
+    // column number of the call site
+    // null for native call site
+    columnNumber: 4,
+
+    // a StackTrace object parsed from the async stack trace
+    // if the Error object is prep'd with async stack trace
+    // otherwise undefined
+    asyncOrigin: <#StackTrace>,
+
+    // a StackFrame object of the eval call site
+    // if the call site in a function compiled in eval or new Function
+    // otherwise null
+    get evalOrigin(),
+
+    // function name
+    get functionName(),
+
+    // formatted string of the function location
+    // depends on options.formatter
+    get source()
+}
+```
+
+### traceable.Formatter([options])
+
+Provides means to customize the formatted output of the stack trace.
+
+There is two built-in formatters, `default` and `native`.
+
+- `traceable.Formatter.default` (a.k.a inspector style):
+```
+  (anonymous function) @ /trace-test/anon.js:2
+  MyClass.method       @ myclass/myclass.js:210
+  dynamic_function     @ <anonymous>:2 eval at myclass/helper.js:50
+```
+
+- `traceable.Formatter.native`
+```
+  at /trace-test/anon.js:2:10
+  at MyClass.extend.myMethod (/trace-test/node_modules/myclass/myclass.js:210:4)
+  at dynamic_function (eval at /trace-test/node_modules/myclass/helper.js:50:12, <anonymous>:2:4)
+```
+
+You can create your own formatter by defining functions
+the handle different components of a stack trace.
+
+All options are **optional**, if not specified the `native` style is used.
+The second parameter `options` is the options passed to `traceable()`.
+
+Each function should return a string.
+
+```javascript
+new traceable.Formatter({
+    // format the entire stack trace
+    // called by StackTrace.toString
+    formatTrace: function (stackTrace, options) { ... },
+
+    // format each call site
+    formatFrame: function (stackFrame, options) { ... },
+
+    // format the location of call site
+    // called by StackFrame.source
+    formatSource: function (stackFrame, options) { ... },
+
+    // format eval site which is appended to the source
+    // called by StackFrame.source
+    formatEvalOrigin: function (stackFrame, options) { ... },
+
+    // format async marker and the whole async stack trace
+    // called by StackFrame.toString
+    // see async stack trace example below
+    formatAsyncOrigin: function (stackTrace, options) { ... }
+})
+```
+
+### traceable.prepAsyncStack([skip])
+
+Enable more verbose stack trace in async operation.
+
+Similar to [long-stack-trace](https://www.npmjs.com/package/long-stack-trace)
+but instead of wrapping async function such as `setTimeout` and `EventEmitter.on`,
+it records the current stack trace and return a prep function.
+
+```javascript
+function somethingAsync() {
+    var prep = traceable.prepAsyncStack();  // returns a function
+    setTimeout(function () {
+        var err = new Error();
+        // the stack trace captured by prepAsyncStack()
+        // is assigned to the Error object
+        throw prep(err);
+    });
+}
+process.on('uncaughtException', function(err){
+    // recognizes the prep'd async stack
+    console.log(traceable(err));
+});
+```
+would output the following to console:
+```
+    at async.js:6:19
+    [async]
+    at somethingAsync (async.js:2:16)
+    at someOtherFunction (main.js:40:4)
+```
+
+#### Options
+
+`skip`: Number of frames to skip
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 misonou
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ traceable.js
@@ -0,0 +1,279 @@
+/*jshint node:true */
+
+var sep = require('path').sep;
+var needles = {};
+var reFn = /^(?:([^\.\[\(\s]+)(?:\.+([^\.\[\(\s]+))*)?(?:\s*\[as\s*([^\]]+)\])?$/;
+var reEval = /^(?:eval |\s*)?at (?:(new )?((?:\((?!eval at )|[^\(])+) \()?(?:native|null|(?:(.+), )?(.+):(\d+):(\d+))\)?$/;
+
+function define(obj, prop, value) {
+    Object.defineProperty(obj, prop, {
+        value: value,
+        configurable: true
+    });
+}
+
+function copy(dst, src) {
+    for (var i in src) {
+        dst[i] = src[i];
+    }
+    return dst;
+}
+
+function repeat(c, len) {
+    return Array.prototype.join.call({
+        length: len + 1
+    }, c);
+}
+
+function prepRawFrames(str) {
+    return str.split('\n').slice(1, -1).map(function (v) {
+        return {
+            rawString: v
+        };
+    });
+}
+
+function computeNeedles(filename) {
+    filename = (filename || '').toLowerCase();
+    return needles[filename] || (function (cache) {
+        var arr = filename.split(sep);
+        var idx = (arr.lastIndexOf('node_modules') + 1) || (arr.lastIndexOf('.node_modules') + 1);
+        var needles = [filename];
+        if (idx > 0) {
+            arr.slice(idx).forEach(function (v) {
+                needles.splice(1, 0, needles[1] ? needles[1] + '/' + v : v);
+            });
+        }
+        cache[filename] = needles;
+        return needles;
+    }(needles));
+}
+
+function isBlackBoxed(filename, arr) {
+    var needles = computeNeedles(filename);
+    return needles.some(function (v) {
+        return arr.indexOf(v) >= 0;
+    });
+}
+
+function normalizeFunctionName(f, t) {
+    // V8 gives functions in object literals and named function assigned elsewhere
+    // as namespaced '$.extend.myFunction' or aliased 'Class.method [as anotherName]'
+    // use the resolved name or strip the namespaces because if there was a name the name should be verbose enough
+    f = (reFn.test(f), (RegExp.$3 || RegExp.$2 || RegExp.$1 || ''));
+    if (f === '<anonymous>') {
+        f = '';
+    }
+
+    // choose the outermost namespace as the type name if any
+    // if type name is 'Object' check whether the function belongs to Object or Object.prototype
+    // it is so often a function is passed by an object literal and that would resolve as Object.functionName
+    // replace Object with a question mark (?) to indicate an anonymous object literal
+    t = t || (RegExp.$2 ? RegExp.$1 : '');
+    if (t === 'Object' && (!Object[f] || !Object.prototype[f])) {
+        t = '?';
+    }
+    return (t && f && (t + '.')) + f;
+}
+
+function captureV8StackTrace(belowFn, skipFrame) {
+    var oldLimit = Error.stackTraceLimit;
+    var v8Handler = Error.prepareStackTrace;
+    try {
+        var dummyObject = {};
+        Error.stackTraceLimit = Infinity;
+        Error.prepareStackTrace = function (obj, st) {
+            return st;
+        };
+        Error.captureStackTrace(dummyObject, belowFn || captureV8StackTrace);
+        return dummyObject.stack.slice(skipFrame || 0);
+    } finally {
+        Error.prepareStackTrace = v8Handler;
+        Error.stackTraceLimit = oldLimit;
+    }
+}
+
+function prepAsyncStack(skipFrame) {
+    var st = new Error();
+    var async;
+    setImmediate(function () {
+        async = true;
+    });
+    return function (err) {
+        if (err && async) {
+            err.asyncStack = err.asyncStack || (function (arr) {
+                arr.splice(1, +skipFrame || 0);
+                return arr.join('\n');
+            }(st.stack.split('\n')));
+        }
+        return err;
+    };
+}
+
+function StackTrace(frames, options) {
+    define(this, '_options', options);
+    Array.prototype.push.apply(this, frames.map(function (v) {
+        return new StackFrame(v, options);
+    }).filter(function (v) {
+        return !Array.isArray(options.blackbox) || !isBlackBoxed(v.fileName, options.blackbox);
+    }));
+}
+StackTrace.prototype = Object.create(Array.prototype);
+StackTrace.prototype.toString = function () {
+    return this._options.formatter.formatTrace(this, this._options);
+};
+
+function StackFrame(input, options) {
+    if (input.rawString || typeof input === 'string') {
+        this.rawString = input.rawString || input;
+        if (reEval.test(input.rawString || input)) {
+            var fn = RegExp.$2;
+            var posDot = fn.indexOf('.');
+            define(this, '_evalOrigin', RegExp.$3 || null);
+            copy(this, {
+                native: !RegExp.$4,
+                isConstructor: !!RegExp.$1,
+                typeName: posDot > 0 ? fn.substr(0, posDot) : '',
+                rawFunctionName: fn.substr(posDot + 1),
+                fileName: RegExp.$4 || null,
+                lineNumber: RegExp.$5 || null,
+                columnNumber: RegExp.$6 || null
+            });
+        }
+    } else {
+        define(this, '_evalOrigin', input.isEval() ? input.getEvalOrigin() : null);
+        copy(this, {
+            rawString: input.toString(),
+            native: input.isNative(),
+            isConstructor: input.isConstructor(),
+            typeName: input.getTypeName() || '',
+            rawFunctionName: input.getFunctionName() || '',
+            fileName: input.isNative() ? null : input.getFileName() || '<anonymous>',
+            lineNumber: input.getLineNumber() || null,
+            columnNumber: input.getColumnNumber() || null
+        });
+    }
+    if (input.asyncOrigin) {
+        this.asyncOrigin = new StackTrace(input.asyncOrigin, options);
+    }
+    if (!options.showFullPath) {
+        this.fileName = computeNeedles(this.fileName)[1] || this.fileName;
+    }
+    define(this, '_options', options);
+}
+StackFrame.prototype = {
+    get evalOrigin() {
+        if (typeof this._evalOrigin === 'string') {
+            define(this, '_evalOrigin', new StackFrame(this._evalOrigin, this._options));
+        }
+        return this._evalOrigin;
+    },
+    get functionName() {
+        if (!this.hasOwnProperty('_functionName')) {
+            var functionName = (this._options.showRawFunctionName ? (this.typeName && this.typeName + '.') + this.rawFunctionName :
+                normalizeFunctionName(this.rawFunctionName, this.typeName) ||
+                this._options.anonString ||
+                this._options.formatter.anonString || '');
+            define(this, '_functionName', (this.isConstructor && 'new ' || '') + functionName);
+        }
+        return this._functionName;
+    },
+    get source() {
+        if (!this.hasOwnProperty('_source')) {
+            var formatter = this._options.formatter;
+            var str = formatter.formatSource(this, this._options);
+            if (this._options.showEvalOrigin && this.evalOrigin) {
+                str += formatter.formatEvalOrigin(this.evalOrigin, this._options);
+            }
+            define(this, '_source', str);
+        }
+        return this._source;
+    },
+    toString: function () {
+        if (!this.hasOwnProperty('_string')) {
+            var formatter = this._options.formatter;
+            var str = this._options.indent + formatter.formatFrame(this, this._options);
+            if (this._options.showAsyncOrigin && this.asyncOrigin) {
+                str += formatter.formatAsyncOrigin(this.asyncOrigin, this._options);
+            }
+            define(this, '_string', str);
+        }
+        return this._string;
+    }
+};
+
+function Formatter(options) {
+    copy(this, options);
+}
+Formatter.prototype = {
+    anonString: '',
+    formatTrace: function (v, options) {
+        return v.join('\n');
+    },
+    formatFrame: function (v, options) {
+        return 'at ' + (v.functionName ? v.functionName + ' (' + v.source + ')' : v.source);
+    },
+    formatSource: function (v, options) {
+        return this.native ? 'native' : v.fileName + ':' + v.lineNumber + (options.showColumnNumber ? ':' + v.columnNumber : '');
+    },
+    formatEvalOrigin: function (v, options) {
+        return ' eval at ' + v.source;
+    },
+    formatAsyncOrigin: function (v, options) {
+        return '\n' + options.indent + '[async]\n' + v;
+    }
+};
+Formatter.native = new Formatter();
+Formatter.default = new Formatter({
+    anonString: '(anonymous function)',
+    formatTrace: function (v, options) {
+        function getMaxLength(trace) {
+            trace.forEach(function (frame) {
+                options.fmax = Math.max(frame.functionName.length, options.fmax || 0);
+                if (frame.asyncOrigin) {
+                    getMaxLength(frame.asyncOrigin);
+                }
+            });
+        }
+        getMaxLength(v);
+        return Formatter.prototype.formatTrace.apply(this, arguments);
+    },
+    formatFrame: function (v, options) {
+        return v.functionName + repeat(' ', options.fmax - v.functionName.length) + ' @ ' + v.source.replace(/\\/g, '/');
+    }
+});
+
+module.exports = exports = function traceable(v, options) {
+    if (!options && typeof v === 'object' && !v.stack) {
+        options = v;
+        v = 0;
+    }
+    options = copy({}, options);
+    if (!options.hasOwnProperty('showEvalOrigin')) {
+        options.showEvalOrigin = true;
+    }
+    if (!options.hasOwnProperty('showAsyncOrigin')) {
+        options.showAsyncOrigin = true;
+    }
+    options.formatter = options.formatter || Formatter.default;
+    options.indent = isNaN(+options.indent) ? options.indent || '    ' : repeat(' ', options.indent || 1);
+
+    var frames;
+    if (v && v.stack) {
+        frames = prepRawFrames(v.stack);
+        if (v.asyncStack) {
+            frames[frames.length - 1].asyncOrigin = prepRawFrames(v.asyncStack);
+        }
+    } else if (typeof v === 'string') {
+        frames = prepRawFrames(v);
+    } else if (+v === v) {
+        frames = captureV8StackTrace(traceable, v);
+    } else {
+        frames = captureV8StackTrace(v || traceable);
+    }
+    return new StackTrace(frames, options);
+};
+
+exports.Formatter = Formatter;
+exports.trace = exports;
+exports.prepAsyncStack = prepAsyncStack;
